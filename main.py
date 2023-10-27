02febd8c3cbd4df8430ab409ce7f7451c3a1478c
Negarnbkhti42 <norob35@gmail.com>
2023-10-27 13:20:21 +0330

    Init interface
    Create a part for getting user input

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("hello world!")
+def main():
+    print("Hello World!")
+    while True: 
+        guessed_word = input('type your guess (empty for exit):')
+        if len(guessed_word) == 0:
+            break
+        
+        if len(guessed_word) != 5:
+            print('your guess should have 5 letters')
+
+if __name__ == '__main__':
+    main()
