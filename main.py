f42d3cbf8ed46021b901195d0a3d3477372b18cc
Negarnbkhti42 <norob35@gmail.com>
2023-10-27 13:14:17 +0330

    Read words
    Read data from words file and store in program

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello world!")
+def main():
+    print("Hello World!")
+    words = []
+    with open('./assets/valid-wordle-words.txt', 'r', encoding= 'UTF-8') as file:
+        words = [line.strip() for line in file.readlines()]
+    print(words[:5])
+    
+
+if __name__ == "__main__":
+    main()
