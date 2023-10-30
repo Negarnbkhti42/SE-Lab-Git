06d00f92850d7a1c5a49b18efa963d63d8101fdc
Negarnbkhti42 <norob35@gmail.com>
2023-10-30 21:52:51 +0330

    Add processing logic

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+import random
 print("hello world!")
 
+def load_valid_words(path):
+    words = []
+    with open(path, 'r', encoding='UTF-8') as file:
+        words = [word.strip() for word in file.readlines()]
+
+    return words
+
+def main():
     valid_words = load_valid_words('./assets/valid-wordle-words.txt')
-    print(valid_words[:5])
+    answer = random.choice(valid_words)
 
     while True:
         guessed_word = input('Type your guess (press Enter to exit): ')
@@ -13,6 +22,12 @@ print("hello world!")
             print('Your guess should have exactly 5 letters.')
         else:
             # Your logic for processing the guessed word goes here.
+            if guessed_word == answer:
+                print('you guessed right!')
+                break
+            else:
+                print("try again!")
+    print('the answer was:', answer)
 
 if __name__ == '__main__':
     main()
